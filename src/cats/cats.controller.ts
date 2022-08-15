1234ae19f3ef859c637a14cc960047522ee800dc
최준성 <jay@jay.local>
2022-08-15 18:49:47 +0900

    middleware pattern, interceptor pattern, pipe pattern

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,20 +1,24 @@
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post, Put, HttpException, UseFilters } from '@nestjs/common';
-import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { Controller, Delete, Get, Patch, Post, Put, HttpException, UseFilters, Param, ParseIntPipe, UseInterceptors } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { PositiveIntPipe } from 'src/pipes/positiveInt.pite';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
 	constructor(private readonly catsService: CatsService) {}
 
 	@Get()
-	@UseFilters(HttpExceptionFilter)
 	allCats() {
-		throw new HttpException('api is broken', 401);
-		return 'all Cats';
+		console.log('hello controller');
+		return { cats: 'get all cat api' };
 	}
 
 	@Get('/:id')
-	getCatById() {
+	getCatById(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+		// console.log(param);
 		return 'Cat';
 	}
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const port = 8000;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(port);
-    Logger.log(`http://localhost:${port}`, 'App Running');
+	const app = await NestFactory.create(AppModule);
+	app.useGlobalFilters(new HttpExceptionFilter());
+	await app.listen(port);
+	Logger.log(`http://localhost:${port}`, 'App Running');
 }
 
 bootstrap();
